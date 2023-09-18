c9a0ec0029abb22e44ebff19f30f3f69e80d6300
D Kirago Van Der Plas <daniel_k_vdp@hotmail.com>
2023-09-18 23:14:33 +0300

    Added docstrings to my_function.py

---
+++ my_function.py
@@ -0,0 +1,14 @@
+
+def calculate_total_price(ticket_price, quantity):
+    """
+    Calculate the total price of tickets based on the ticket price and quantity.
+
+    Parameters:
+    item_price (float): The price of a single item.
+    quantity (int): The quantity of items to be purchased.
+
+    Returns:
+    float: The total price.
+    """
+    total_price = ticket_price * quantity
+    return total_price
